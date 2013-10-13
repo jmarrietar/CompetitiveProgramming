486655e43c275235627fffb40789deb2d751451e
Jose Miguel Arrieta <jmarrietar@gmail.com>
2013-10-13 12:48:41 -0500

    oCEANdeepfgddwerjr

--- RayThroughGlasses/src/raythroughglasses/RayThroughGlasses.java
+++ RayThroughGlasses/src/raythroughglasses/RayThroughGlasses.java
@@ -4,16 +4,50 @@
  */
 package raythroughglasses;
 
+import java.io.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+
 /**
  *
  * @author EQUIPO JOSE
  */
 public class RayThroughGlasses {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+/*
+   public static BigInteger FibRecursive (BigInteger n ){
+       
+       BigInteger Dos = new BigInteger("2");
+       BigInteger Uno = new BigInteger("1");
+       if (n.equals(BigInteger.ONE)){
+           return Dos; 
+       }
+       if (n.equals(BigInteger.ZERO)) {
+           return Uno; 
+       }
+       return FibRecursive(n.subtract(Dos)).add(FibRecursive(n.subtract(Uno))); 
+   }
+           
+    */
+    public static void main(String[] args) throws FileNotFoundException {
+       
+        ArrayList <BigInteger> Fib = new ArrayList<BigInteger>(); 
+        
+        Fib.add( new BigInteger("1"));
+        Fib.add( new BigInteger("2"));
+    
+        for (int i =2; i<=1000;i++){
+           
+           Fib.add(Fib.get(i-1).add(Fib.get(i-2)));
+        }
+      
+        BufferedReader br = new BufferedReader (new FileReader ("in.txt"));
+      //  BufferedReader br = new BufferedReader (new InputStreamReader (System.in)); 
+        while (br.ready()){
+            int N = Integer.parseInt(br.readLine()); 
+                System.out.println(Fib.get(N)); 
+            
+        }
+    
+        
     }
 }
